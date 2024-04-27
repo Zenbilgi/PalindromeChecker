8aab3f56d22ce6fbb1aec5b5c9f3f611a916edbe
baytas <mrtas@hotmail.com>
2024-04-28 01:21:32 +0300

    Initial commit

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using System.Text.RegularExpressions;
 
+
+Console.WriteLine("Palindrome Checker");
+Console.WriteLine("------------------");
+Console.Write("Enter a string: ");
+string input = Console.ReadLine();
+
+// Remove punctuation marks and spaces
+string cleanedInput = StringOperation.RemovePunctuationAndSpaces(input);
+
+// Reverse the cleaned string
+string reversedInput = StringOperation.ReverseString(cleanedInput);
+
+// Check if the cleaned string is equal to its reverse
+bool isPalindrome = cleanedInput.Equals(reversedInput, StringComparison.OrdinalIgnoreCase);
+
+// Output the result
+Console.WriteLine();
+Console.WriteLine($"Original string: {input}");
+Console.WriteLine($"Cleaned string: {cleanedInput}");
+Console.WriteLine($"Reversed string: {reversedInput}");
+Console.WriteLine($"Is Palindrome: {isPalindrome}");
+
+Console.ReadKey();
---
+++ Palindrome/StringOperation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public class StringOperation
+{
+
+    public static string RemovePunctuationAndSpaces(string input)
+    {
+        // Remove punctuation marks using regular expression
+        string withoutPunctuation = Regex.Replace(input, "[^a-zA-Z0-9]", "");
+
+        // Remove spaces
+        string withoutSpaces = withoutPunctuation.Replace(" ", "");
+
+        return withoutSpaces;
+    }
+
+    public static string ReverseString(string input)
+    {
+        // return new string(input.Reverse().ToArray());
+
+        int len = input.Length;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = len - 1; i >= 0; i--)
+        {
+            sb.Append(input[i]);
+        }
+
+        return sb.ToString();
+    }
+
+}
+
